7dd5aa412e0f2871347402431fb6da80d3b70027
Ashi <ashigoel128@gmail.com>
2024-06-18 10:34:17 +0530

    api related

--- src/index.js
+++ src/index.js
@@ -6,8 +6,15 @@ dotenv.config({
 import connectDB from "./db/connectDB.js";
 
 
-connectDB();
-
+connectDB()
+.then(() =>{
+    app.listen(process.env.PORT || 5000, () =>{
+        console.log(`Server is running at PORT ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log(`Mongo DB connection failed`, err);
+})
 
 
 

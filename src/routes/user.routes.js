c3a84e769d125f443d739ec84f6dd390f67f0f66
Ashi <ashigoel128@gmail.com>
2024-06-19 11:19:37 +0530

    login and logout apis completed

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { login, logout, registerUser } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js"
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router()
 router.route("/register").post(
@@ -16,4 +17,8 @@ router.route("/register").post(
     ]),
     registerUser
 )
+
+router.route("/login").post(login)
+
+router.route("/logout").post(verifyJWT, logout)
 export default router
